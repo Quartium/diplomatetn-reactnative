9070086a879f2066283171bd2c5c1e5db9685bab
Quartium <firas404@gmail.com>
2023-07-05 22:38:30 +0100

    Optimized and updated the code for Home screen and category component

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,32 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { View, Text, ImageBackground, TouchableOpacity, FlatList, StyleSheet, useWindowDimensions } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Moment from "moment";
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbwRSJrG748NCEZvBANIhMJM1uwu4QiITrPmqLTRNN91YjqLTxNYgb-Z07iJQ4RL7jc14g/exec';
+
+const formatDate = (dateString) => {
+    const date = Moment(dateString);
+    return `Publié le ${date.format('DD MMMM YYYY')}`;
+};
 
 const Category = ({ item, categoryId, onPress }) => {
     const [categoryPosts, setCategoryPosts] = useState([]);
     const [categoryName, setCategoryName] = useState("");
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     const window = useWindowDimensions();
 
-    useEffect(() => {
-        if (categoryId) {
-            fetch(`${API_URL}?categoryId=${categoryId}`)
-                .then(response => response.json())
-                .then(data => setCategoryPosts(data))
-                .catch(error => console.error(error));
-
-            fetch(`${API_URL}?categories&categoryId=${categoryId}`)
-                .then(response => response.json())
-                .then(data => setCategoryName(data.name))
-                .catch(error => console.error(error));
-        }
-    }, [categoryId]);
-
-    const styles = StyleSheet.create({
+    const styles = useMemo(() => StyleSheet.create({
         container: {
-            width: window.width * 0.8,
+            width: window.width * 0.5,
             height: window.height * 0.3,
-            marginRight: window.width * 0.1,
+            marginRight: 24,
             borderRadius: 10,
             overflow: 'hidden',
         },
@@ -35,30 +31,93 @@ const Category = ({ item, categoryId, onPress }) => {
             justifyContent: 'flex-end',
         },
         textContainer: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            backgroundColor: '#F2F2F2',
             padding: 10,
         },
         text: {
-            fontSize: 24,
-            color: 'white',
+            fontSize: 16,
             fontWeight: 'bold',
+            color: '#192344'
         },
-    });
+        date: {
+            fontSize: 16,
+            color: '#192344'
+        },
+        loadingText: {
+            color: 'red',
+        },
+        categoryContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginLeft: 24,
+            marginTop: 12
+        },
+        categoryName: {
+            fontSize: 18,
+            color: '#D91F3C',
+            marginLeft: 12,
+        },
+        flatListContainer: {
+            paddingVertical: 24,
+            paddingLeft: 24
+        },
+    }), [window.width, window.height]);
+
+    useEffect(() => {
+        setLoading(true);
+        if (categoryId) {
+            fetch(`${API_URL}?categoryId=${categoryId}`)
+                .then(response => response.json())
+                .then(data => setCategoryPosts(data))
+                .catch(error => {
+                    console.error(error);
+                    setError('An error occurred while fetching posts');
+                })
+                .finally(() => setLoading(false));
+
+            fetch(`${API_URL}?categories&categoryId=${categoryId}`)
+                .then(response => response.json())
+                .then(data => setCategoryName(data.name))
+                .catch(error => {
+                    console.error(error);
+                    setError('An error occurred while fetching category name');
+                });
+        }
+    }, [categoryId]);
+
+    if (loading) {
+        return <Text style={styles.loadingText}>Loading...</Text>;
+    }
+
+    if (error) {
+        return <Text>{error}</Text>;
+    }
 
     return (
         <View>
-            <Text>{categoryName}</Text>
+            <View
+                style={styles.categoryContainer}>
+                <MaterialCommunityIcons
+                    name="label"
+                    color="#D91F3C"
+                    size={30} />
+                <Text
+                    style={styles.categoryName}>{categoryName}</Text>
+            </View>
             <FlatList
+                contentContainerStyle={styles.flatListContainer}
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 data={categoryPosts}
                 renderItem={({ item }) => (
                     <TouchableOpacity onPress={onPress} style={styles.container}>
                         <ImageBackground source={{ uri: item.featured_image }} style={styles.image}>
-                            <View style={styles.textContainer}>
-                                <Text style={styles.text}>{item.title}</Text>
-                            </View>
                         </ImageBackground>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.text} numberOfLines={2}>{item.title}</Text>
+                            <Text style={styles.date}>{formatDate(item.date)}</Text>
+                        </View>
                     </TouchableOpacity>
                 )}
                 keyExtractor={item => item.id.toString()}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { View, Text, ImageBackground, TouchableOpacity, FlatList, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Moment from "moment";
@@ -7,41 +7,24 @@ import Category from '../components/Category';
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbwRSJrG748NCEZvBANIhMJM1uwu4QiITrPmqLTRNN91YjqLTxNYgb-Z07iJQ4RL7jc14g/exec?posts=true';
 
+const formatDate = (dateString) => {
+    const date = Moment(dateString);
+    return `Publié le ${date.format('DD MMMM YYYY')}`;
+};
+
 const HomeScreen = () => {
     const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     const navigation = useNavigation();
     const window = useWindowDimensions();
 
-    useEffect(() => {
-        fetch(API_URL)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (Array.isArray(data)) {
-                    setData(data.slice(0, 3)); // Get the three latest posts
-                } else {
-                    console.error('Data is not an array of posts.');
-                }
-            })
-            .catch(error => {
-                console.error('Fetch error: ', error);
-            });
-    }, []);
-
-    const formatDate = (dateString) => {
-        const date = Moment(dateString);
-        return `Publié le ${date.format('DD MMMM YYYY')}`;
-      };
-    
-    const styles = StyleSheet.create({
+    const styles = useMemo(() => StyleSheet.create({
         container: {
-            width: window.width * 0.8,
+            width: window.width * 0.65,
             height: window.height * 0.3,
-            marginRight: window.width * 0.1,
+            marginRight: 24,
             borderRadius: 10,
             overflow: 'hidden',
         },
@@ -50,11 +33,13 @@ const HomeScreen = () => {
             justifyContent: 'flex-end',
         },
         textContainer: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
             padding: 10,
+            justifyContent: 'flex-end'
         },
         title: {
-            fontSize: 24,
+            fontSize: 16,
             color: 'white',
             fontWeight: 'bold',
         },
@@ -62,12 +47,65 @@ const HomeScreen = () => {
             fontSize: 16,
             color: 'white',
         },
-    });
+        loadingText: {
+            fontSize: 18,
+            textAlign: 'center',
+            marginTop: 50,
+        },
+        errorText: {
+            fontSize: 18,
+            textAlign: 'center',
+            marginTop: 50,
+            color: 'red',
+        },
+        mainContainer: {
+            backgroundColor: 'white',
+        },
+        flatListContainer: {
+            backgroundColor: '#F2F2F2',
+            paddingVertical: 24,
+            paddingLeft: 24
+        },
+    }), [window.width, window.height]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(API_URL);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                if (Array.isArray(data)) {
+                    setData(data.slice(0, 3)); // Get the three latest posts
+                }
+            } catch (error) {
+                console.error('Fetch error: ', error);
+                setError('An error occurred while fetching posts');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <Text style={styles.loadingText}>Loading...</Text>;
+    }
+
+    if (error) {
+        return <Text style={styles.errorText}>{error}</Text>;
+    }
 
     return (
         <ScrollView>
-            <View style={{}}>
+            <View style={styles.mainContainer}>
                 <FlatList
+                    contentContainerStyle={styles.flatListContainer}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     data={data}
@@ -79,7 +117,7 @@ const HomeScreen = () => {
                             <ImageBackground source={{ uri: item.featured_image }} style={styles.image}>
                                 <View style={styles.textContainer}>
                                     <Text>À la une</Text>
-                                    <Text style={styles.title}>{item.title}</Text>
+                                    <Text style={styles.title} numberOfLines={2}>{item.title}</Text>
                                     <Text style={styles.date}>{formatDate(item.date)}</Text>
                                 </View>
                             </ImageBackground>
@@ -87,8 +125,10 @@ const HomeScreen = () => {
                     )}
                     keyExtractor={item => item.id.toString()}
                 />
-                <Category categoryId={2} />
-                <Category categoryId={3} />
+                <Category categoryId={7} />
+                <Category categoryId={8} />
+                <Category categoryId={9} />
+                <Category categoryId={10} />
             </View>
         </ScrollView>
     );
